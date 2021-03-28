5170ddd76504a28b8f9f48beb47408f5b4167ad6
chapdel <chapdel.kamga2@gmail.com>
2021-03-28 16:51:15 +0100

    Add default message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
@@ -23,6 +15,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return response()->json([
+            "status" => 200,
+            "message" => trans("Welcome on my great laravel API")
+        ]);
     }
 }
